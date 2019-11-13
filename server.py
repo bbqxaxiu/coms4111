0333c2c39df82787d1ca12d444ad72cf744e44f3
@vladverba <vladverba.vv@gmail.com>
2019-11-12 19:59:30 -0500

    vlad's server.py
    following thing works

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ app = Flask(__name__, template_folder=tmpl_dir)
 #
 # The following is a dummy URI that does not connect to a valid database. You will need to modify it to connect to your Part 2 database in order to use the data.
 #
-# XXX: The URI should be in the format of:
+# XXX: The URI should be in the format of: 
 #
 #     postgresql://USER:PASSWORD@35.243.220.243/proj1part2
 #
@@ -53,7 +53,7 @@ engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'
 @app.before_request
 def before_request():
   """
-  This function is run at the beginning of every web request
+  This function is run at the beginning of every web request 
   (every time you enter an address in the web browser).
   We use it to setup a database connection that can be used throughout the request.
 
@@ -87,7 +87,7 @@ def teardown_request(exception):
 #       @app.route("/foobar/", methods=["POST", "GET"])
 #
 # PROTIP: (the trailing / in the path is important)
-#
+# 
 # see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
 # see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
 #
@@ -116,6 +116,10 @@ def index():
     names.append(result['name'])  # can also be accessed using result[0]
   cursor.close()
 
+#cursor = g.conn.execute("SELECT handle FROM Users WHERE handle = name") #pseudo
+
+#cursor = g.conn.execute("SELECT ")
+
   #
   # Flask uses Jinja templates, which is an extension to HTML where you can
   # pass data to a template and dynamically generate HTML based on the data
@@ -125,14 +129,14 @@ def index():
   # You can see an example template in templates/index.html
   #
   # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be
+  # for example, "data" key in the context variable defined below will be 
   # accessible as a variable in index.html:
   #
   #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
   #     <div>{{data}}</div>
-  #
+  #     
   #     # creates a <div> tag for each element in data
-  #     # will print:
+  #     # will print: 
   #     #
   #     #   <div>grace hopper</div>
   #     #   <div>alan turing</div>
@@ -144,6 +148,7 @@ def index():
   #
   context = dict(data = names)
 
+
   #
   # render_template looks in the templates/ folder for files.
   # for example, the below file reads template/index.html
@@ -152,7 +157,7 @@ def index():
 
 #
 # This is an example of a different path.  You can see it at:
-#
+# 
 #     localhost:8111/another
 #
 # Notice that the function name is another() rather than index()
@@ -160,7 +165,6 @@ def index():
 #
 @app.route('/another')
 def another():
-  create_tweet("vladverba", "vlad's test text", "vlad's test media")
   return render_template("another.html")
 
 # Creates a tweet for a given handle.
@@ -169,11 +173,11 @@ def create():
     handle = request.args.get('handle')
     text = request.args.get('text')
     media = request.args.get('media')
-
+    
     handle_exists = check_if_handle_exists(handle)
     if(handle_exists):
         create_tweet(handle, text, media)
-
+    
     return redirect('/')
 
 # Displays a user's tweets
@@ -196,45 +200,52 @@ def display():
     if(handle_exists):
         following = get_users_someone_follows(handle)
         tweets = get_tweets_from_users(following)
-        return render_template("tweets_of_people_you_follow.html", tweets=tweets)
+        return render_template("tweets.html", tweets=tweets)
     else:
         return redirect('/')
 
-# Example of adding new data to the database
+# Follow a user
 @app.route('/add', methods=['POST'])
 def add():
-  name = request.form['name']
-  g.conn.execute('INSERT INTO test(name) VALUES (%s)', name)
-  return redirect('/')
+  followerHandle = request.form['followerHandle']
+  followingHandle = request.form['followedHandle']
+  #print(followingHandle)
+  #print(followerHandle)
+  followerHandle_exists = check_if_handle_exists(followerHandle)
+  followingHandle_exists = check_if_handle_exists(followingHandle)
+  if(followerHandle_exists and followingHandle_exists):
+      g.conn.execute("INSERT INTO following VALUES (%s, %s)", followerHandle, followingHandle)
+      return redirect('/')
+  else:
+      return redirect('/')
+
 
 @app.route('/login')
 def login():
     abort(401)
     this_is_never_executed()
 
-
-# HELPER FUNCTIONS
-
 # generates random date time for tweets (always will be in 2020)
 def generate_date_time():
     month = randint(10, 12)
     day = randint(10, 28)
     year = randint(2020, 2030)
-
+    
     hour = randint(00, 23)
     minute = randint(00, 59)
     second = randint(00, 59)
-
+    
     return str(year) + "-" + str(month) + "-" + str(day) + " " + str(hour) + ":" + str(minute) + ":" + str(second)
 
-# SQL QUERIES
+
+# queries
 
 # creates a tweet under a handle (calls on create_content)
 def create_tweet(handle, text, media):
     id = randint(10000000000, 99999999999)
     while(g.conn.execute("SELECT * from tweets_with_content t where CAST(t.tid as bigint)=%s", id).fetchone() is not None):
         id = randint(10000000000, 99999999999)
-
+    
     like_num = randint(0, 100000)
     retweet_num = randint(0, 100000)
     date_time = generate_date_time()
@@ -253,31 +264,32 @@ def create_content(text, media):
 
 # checks if a handle exists
 def check_if_handle_exists(handle):
-    record = g.conn.execute("SELECT * from users u where u.handle=%s", handle).fetchone()
+    cursor = g.conn.execute("SELECT * from users u where u.handle=%s", handle)
+    record = cursor.fetchone()
     if(record is None):
         return False
     else:
         return True
 
-# returns a list of people someone follows
 def get_users_someone_follows(handle):
     cursor = g.conn.execute("SELECT * from following f where f.follower=%s", handle)
     following = []
     for record in cursor:
         following.append(record['followed'])
-
+    
     return following
 
-# returns tweets that a user (a list of users) has (have) created
+# returns a a dict. of tweets created from a list of users
 def get_tweets_from_users(users):
+    
     tweets={}
     for person in users:
+        # all tweets of the person they follow
         cursor = g.conn.execute("SELECT * from tweets_with_content t where t.handle=%s", person)
         for record in cursor:
             media = g.conn.execute("SELECT media from content c where CAST(c.cid as bigint)=%s", record['cid']).fetchone()[0]
             text = g.conn.execute("SELECT text from content c where CAST(c.cid as bigint)=%s", record['cid']).fetchone()[0]
             tweets[record['cid']] = (text, media)
-
     return tweets
 
 
